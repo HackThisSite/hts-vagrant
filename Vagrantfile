1d4b1b44c99e4a100d5f9dd19f320e33f3d3affe
Leroux Cifer <leroux@fezrev.com>
2012-07-13 19:10:43 -0500

    Updated provisioning shell script: phpbrowscap, and more! Vagrantfile contains more network options.

--- Vagrantfile
+++ Vagrantfile
@@ -8,10 +8,18 @@ Vagrant::Config.run do |config|
   # config.vm.box_url = "http://dev.hackthissite.org/vagrant/debian664.box"
 
   config.vm.host_name = "htsdev"
-  config.vm.network :hostonly, "10.13.37.99"
+
+  # Change the 
+  config.vm.network :hostonly, "172.13.37.99"
+  # config.vm.network :hostonly, "192.13.37.99"
+  # config.vm.network :hostonly, "10.13.37.99"
+
+  # Uncomment this to have the vm act as a device on the network you are connected to.
   # config.vm.network :bridged
+
   config.vm.forward_port 80, 8080
   config.vm.share_folder "data", "/data", "./data"
 
-  config.vm.provision :shell, :path => "buildhts.sh"
+  # Hopefully we'll have puppet in place of a shell script soon.
+  config.vm.provision :shell, :path => "provision.sh"
 end
---
+++ provision.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+# Vars
+# Vagrantfile mounts ./data to /data on the vm
+CONFIGS='/data/configs'
+HTS='/data/hackthissite'
+APT='apt-get -f -y --force-yes'
+
+# Add some sources.
+# php5-fpm repo
+echo 'deb http://packages.dotdeb.org stable all' >> /etc/apt/sources.list
+echo 'deb-src http://packages.dotdeb.org stable all' >> /etc/apt/sources.list
+
+# Update, upgrade, install deps.
+# We try to keep upgraded bases on the vagrant boxes hosted on hts
+# so that upgrade doesn't take hours.
+$APT update
+$APT upgrade
+
+# Just some depends
+$APT install git libpcre3-dev sqlite3
+
+# PHP
+$APT install php5-dev php5-fpm php-apc php-pear php5-sqlite
+pecl install apc bbcode mongo
+update-rc.d php5-fpm defaults
+
+# Nginx
+$APT install nginx
+update-rc.d nginx defaults
+
+# MongoDB
+$APT install mongodb
+update-rc.d mongodb defaults
+
+# Redis
+$APT install redis-server redis-doc
+update-rc.d redis defaults
+
+# ElasticSearch - please try to keep the elastic link updated.
+$APT install openjdk-6-jre-headless
+curl -OL https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.19.8.zip
+unzip elasticsearch-* && rm elasticsearch-*.zip
+mv elasticsearch-* /usr/local/elasticsearch
+mv /data/configs/elasticsearch-initscript /etc/init.d/elasticsearch
+chmod +x /etc/init.d/elasticsearch
+update-rc.d elasticsearch defaults
+
+# phpredis
+git clone https://github.com/nicolasff/phpredis.git /tmp/phpredis
+cd phpredis
+phpize; ./configure; make && make installl; cd
+
+# MsgPack for php
+pecl channel-discover php-msgpack.googlecode.com/svn/pecl
+pecl install msgpack/msgpack-beta
+
+# BrowsCap
+cp $CONFIGS/php_browscap.ini /etc/php5/conf.d/
+
+# HackThisSite Setup
+cp $CONFIGS/hts.ini /etc/php5/conf.d/
+cp $CONFIGS/hackthissite-nginx.conf /etc/nginx/sites-available/hackthissite
+rm /etc/nginx/sites-enabled/default
+ln -s /etc/nginx/sites-available/hackthissite /etc/nginx/sites-enabled/
+mkdir $HTS/application/configs/servers/dev.hts
+cp $CONFIGS/override.php $HTS/application/configs/servers/dev.hts/override.php
